daa817780d97753e9b64ca3be3926f1563ad0199
Thanh Vo Minh <thanhvom@imt-soft.com>
2017-12-20 00:30:59 +0700

    Added SearchByText, SearchByLatLon

--- Swift_Networking/Constants.swift
+++ Swift_Networking/Constants.swift
@@ -12,14 +12,18 @@ import UIKit
 struct Constants {
     // MARK: Flickr
     struct Flickr {
-        static let APIScheme = "https://"
+        static let APIBaseURL = "https://api.flickr.com/services/rest/"
+        
+        static let APIScheme = "https"
         static let APIHost = "api.flickr.com"
         static let APIPath = "/services/rest"
         
         static let SearchBBoxHalfWidth = 1.0
         static let SearchBBoxHalfHeight = 1.0
-        static let SearchLatRange = (-90.0, 90.0)
-        static let SearchLonRange = (-180.0, 180.0)
+//        static let SearchLatRange = (-90.0, 90.0)
+//        static let SearchLonRange = (-180.0, 180.0)
+        static let SearchLatRange = (-10.0, 10.0)
+        static let SearchLonRange = (-20.0, 20.0)
     }
     
     // MARK: Flickr Parameter Keys
@@ -34,18 +38,29 @@ struct Constants {
         static let Text = "text"
         static let BoundingBox = "bbox"
         static let Page = "page"
+        
+        // lat & Lng
+        static let Lat = "lat"
+        static let Lon = "lon"
     }
     
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
         static let SearchMethod = "flickr.photos.search"
+        static let PhotosRecentSearchMethod = "flickr.photos.getRecent"
+        
         static let APIKey = "e083f3c57706fd8dc38076ab45804d8d"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
         static let GalleryID = "5704-72157622566655097"
         static let MediumURL = "url_m"
+        static let HighURL = "url_h"
         static let UseSafeSearch = "1"
+        
+        // lat & lng
+        static let Lat = 10.0
+        static let Lon = 106.0
     }
     
     // MARK: Flickr Response Keys
@@ -55,6 +70,7 @@ struct Constants {
         static let Photo = "photo"
         static let Title = "title"
         static let MediumURL = "url_m"
+        static let HighURL = "url_h"
         static let Pages = "pages"
         static let Total = "total"
     }
--- Swift_Networking/ViewController.swift
+++ Swift_Networking/ViewController.swift
@@ -13,16 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var grabImageBt: UIButton!
     
-    let sampleParams = [
-        "id":"1001",
-        "name":"IOS Programming",
-        "pubDate":"2016"
-    ] as [String:AnyObject]
+    @IBOutlet weak var searchTf: UITextField!
+    
+    @IBOutlet weak var latTf: UITextField!
+    @IBOutlet weak var lngTf: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("Escape parameters: \(escapedParameters(parameters: sampleParams))")
+        
     
     }
 
@@ -57,8 +55,7 @@ class ViewController: UIViewController {
             ] as [String:AnyObject]
         
         // create url and request
-        let APIBaseURL = Constants.Flickr.APIScheme + Constants.Flickr.APIHost + Constants.Flickr.APIPath
-        let urlString = APIBaseURL + escapedParameters(parameters: methodParameters)
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(parameters: methodParameters)
         guard let url = URL(string: urlString) else { return }
         var request = URLRequest(url: url)
         
@@ -134,6 +131,214 @@ class ViewController: UIViewController {
         
         // start the task!
         task.resume()
+    }
+    
+    @IBAction func searchByText(_ sender: Any) {
+//        var components = URLComponents()
+//        components.scheme = Constants.Flickr.APIScheme
+//        components.host = Constants.Flickr.APIHost
+//        components.path = Constants.Flickr.APIPath
+//
+//        // Initialize queryItems
+//        components.queryItems = [URLQueryItem]()
+//
+//        let queryItem1 = URLQueryItem(name: Constants.FlickrParameterKeys.Method, value: Constants.FlickrParameterValues.SearchMethod)
+//        let queryItem2 = URLQueryItem(name: Constants.FlickrParameterKeys.APIKey, value: Constants.FlickrParameterValues.APIKey)
+//        let queryItem3 = URLQueryItem(name: Constants.FlickrParameterKeys.Text, value: searchTf.text)
+//        let queryItem4 = URLQueryItem(name: Constants.FlickrParameterKeys.Format, value: Constants.FlickrParameterValues.ResponseFormat)
+//        let queryItem5 = URLQueryItem(name: Constants.FlickrParameterKeys.Extras, value: Constants.FlickrParameterValues.MediumURL)
+//
+//        components.queryItems?.append(queryItem1)
+//        components.queryItems?.append(queryItem2)
+//        components.queryItems?.append(queryItem3)
+//        components.queryItems?.append(queryItem4)
+//        components.queryItems?.append(queryItem5)
+//
+//        print("By URLComponents: ", components.url!)
+        let searchParams = [
+            Constants.FlickrParameterKeys.SafeSearch:Constants.FlickrParameterValues.UseSafeSearch,
+            Constants.FlickrParameterKeys.Text:searchTf.text ?? "",
+            Constants.FlickrParameterKeys.Extras:Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.APIKey:Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.Method:Constants.FlickrParameterValues.SearchMethod,
+            Constants.FlickrParameterKeys.Format:Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback:Constants.FlickrParameterValues.DisableJSONCallback
+            ] as [String : AnyObject]
+        
+        let url = flickrURLFromParameters(searchParams)
+        print(url)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        
+        // create network request
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                self.displayError("There was an error with your request: \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                self.displayError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                self.displayError("No data was returned by the request!")
+                return
+            }
+            
+            // parse the data
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+            } catch {
+                self.displayError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
+                self.displayError("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Are the "photos" and "photo" keys in our result? */
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] else {
+                self.displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' and '\(Constants.FlickrResponseKeys.Photo)' in \(parsedResult)")
+                return
+            }
+            
+            // select a random photo
+            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
+            
+            /* GUARD: Does our photo have a key for 'url_m'? */
+            guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                self.displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoDictionary)")
+                return
+            }
+            
+            // if an image exists at the url, set the image and title
+            let imageURL = URL(string: imageUrlString)
+            if let imageData = try? Data(contentsOf: imageURL!) {
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                    self.flickIv.image = UIImage(data: imageData)
+                    self.titleLbl.text = photoTitle ?? "(Untitled)"
+                }
+            } else {
+                self.displayError("Image does not exist at \(imageURL)")
+            }
+        }
+        
+        // start the task!
+        task.resume()
+    }
+    
+    // MARK: Helper for Creating a URL from Parameters
+    
+    private func flickrURLFromParameters(_ parameters: [String:AnyObject]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+
+    @IBAction func searchByLatLng(_ sender: Any) {
+        let latLonTuples = getLatLon()
+        
+        let searchParams = [
+            Constants.FlickrParameterKeys.SafeSearch:1,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL + ", " + Constants.FlickrParameterValues.HighURL,
+//            Constants.FlickrParameterKeys.BoundingBox:getBbox(),
+            Constants.FlickrParameterKeys.Lat: latLonTuples.0,
+            Constants.FlickrParameterKeys.Lon: latLonTuples.1,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.PhotosRecentSearchMethod,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            ] as [String : AnyObject]
+        
+        let searchURL = Constants.Flickr.APIBaseURL + escapedParameters(parameters: searchParams)
+        print(searchURL)
+        
+        guard let url = URL(string: searchURL) else { return }
+        let request = URLRequest(url: url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard (error == nil) else {
+                self.displayError("\(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let jsonData = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+//                print(jsonData)
+                guard let photosDictionary = jsonData["photos"] as? [String:AnyObject], let photoDictionaries = photosDictionary["photo"] as? [[String:AnyObject]] else { return }
+//                print(photoDictionaries)
+
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoDictionaries.count)))
+                let photoDictionary = photoDictionaries[randomPhotoIndex]
+                if let imageURLString = photoDictionary["url_m"] as? String, let photoTitle = photoDictionary["title"] as? String {
+                    print(imageURLString)
+                    print(photoTitle)
+                    let imageURL = URL(string: imageURLString)
+                    let imageData = try Data(contentsOf: imageURL!)
+                    
+                    performUIUpdatesOnMain {
+                        self.setUIEnabled(true)
+                        self.flickIv.image = UIImage(data: imageData)
+                        self.titleLbl.text = photoTitle
+                    }
+                }
+            } catch let err {
+                self.displayError(err.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    // get lat/lng
+    func getLatLon() -> (Double, Double) {
+        if let lat = Double(latTf.text!), let lon = Double(lngTf.text!) {
+            return (lat, lon)
+        } else {
+            return (Constants.FlickrParameterValues.Lat, Constants.FlickrParameterValues.Lon)
+        }
+    }
+    
+    // get bbox string
+    func getBbox() -> String {
+        if let lat = Double(latTf.text!), let lng = Double(lngTf.text!) {
+            let minLng = min(lng - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+            let minLat = max(lat - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+            
+            let maxLng = min(lng + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+            let maxLat = min(lat + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+            
+            return "\(minLng),\(minLat),\(maxLng),\(maxLat)"
+        } else {
+            return "0, 0, 0, 0"
+        }
     }
     
     // Configure UI
